f469c8f12f3bf035b071ea7036d737501fb10fe6
afelua <elena.semenova_2012@mail.ru>
2018-01-13 15:40:26 +0300

    Fix index.js

--- react_toolbox/src/index.js
+++ react_toolbox/src/index.js
@@ -7,37 +7,22 @@ import Modal from './component/Modal';
 import Progress from './component/Progress';
 import MortgageCalculator from './component/mortgage/index';
 import { Switch, BrowserRouter, Route } from 'react-router-dom';
+import { render } from 'react-dom';
 
 const rootEl = document.getElementById('app');
+render(
+    <AppContainer>
+        <BrowserRouter>
+            <main>
+                <Switch>
+                    <Route exact path='/' component={App}/>
+                    <Route path='/modal' component={Modal}/>
+                    <Route path='/progress' component={Progress}/>
+                    <Route path='/mortgage' component={MortgageCalculator}/>
+                </Switch>
+            </main>
+        </BrowserRouter>
+    </AppContainer>,
+    rootEl
+)
 
-const render = () => {
-    ReactDOM.render((
-        <AppContainer>
-            <BrowserRouter>
-                <main>
-                    <Switch>
-                        <Route exact path='/' component={App}/>
-                        <Route path='/modal' component={Modal}/>
-                        <Route path='/progress' component={Progress}/>
-                        <Route path='/mortgage' component={MortgageCalculator}/>
-                    </Switch>
-                </main>
-            </BrowserRouter>
-        </AppContainer>
-    ), rootEl
-  );
-};
-
-if (process.env.NODE_ENV !== 'production') {
-  overrideComponentTypeChecker((classType, reactElement) => (
-    reactElement && (
-      reactElement.type === classType
-      || reactElement.type.name === classType.displayName
-    )
-  ));
-  if (module.hot) {
-    module.hot.accept('./component/App', render);
-  }
-}
-
-render();
