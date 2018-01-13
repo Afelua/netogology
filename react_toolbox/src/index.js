ef1d2932a25606f7e2270eb7eeb8f1f6ca910794
afelua <elena.semenova_2012@mail.ru>
2018-01-13 15:45:08 +0300

    Fix React Router

--- react_toolbox/src/index.js
+++ react_toolbox/src/index.js
@@ -4,21 +4,21 @@ import App from './component/App';
 import Modal from './component/Modal';
 import Progress from './component/Progress';
 import MortgageCalculator from './component/mortgage/index';
-import { Switch, BrowserRouter, Route } from 'react-router-dom';
+import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
 import { render } from 'react-dom';
 
 const rootEl = document.getElementById('app');
-render(
-        <BrowserRouter>
-            <main>
-                <Switch>
-                    <Route exact path='/' component={App}/>
-                    <Route path='/modal' component={Modal}/>
-                    <Route path='/progress' component={Progress}/>
-                    <Route path='/mortgage' component={MortgageCalculator}/>
-                </Switch>
-            </main>
-        </BrowserRouter>,
-    rootEl
-)
 
+const AppRoot = () =>
+    <Router>
+        <main>
+            <Switch>
+                <Route exact path='/' component={App}/>
+                <Route path='/modal' component={Modal}/>
+                <Route path='/progress' component={Progress}/>
+                <Route path='/mortgage' component={MortgageCalculator}/>
+            </Switch>
+        </main>
+    </Router>;
+
+render(<AppRoot/>, rootEl);
